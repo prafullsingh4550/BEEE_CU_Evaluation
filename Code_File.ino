16011c7aa5c016b077180ff96be8f6bc56f55da0
prafullsingh4550 <55459296+prafullsingh4550@users.noreply.github.com>
2019-10-22 00:03:02 +0530

    Update Code_File.ino

--- Code_File.ino
+++ Code_File.ino
@@ -1,36 +1,36 @@
 void setup()
-{  pinMode(3,OUTPUT);
- pinMode(2,OUTPUT);
-  pinMode(5,INPUT);
-  pinMode(4,INPUT);
+{  pinMode(10,OUTPUT);
+ pinMode(11,OUTPUT);
+  pinMode(8,INPUT);
+  pinMode(9,INPUT);
   Serial.begin(9600);
 }
 
 void loop()
 {
  int d= 0;
-  d =  digitalRead(4);
+  d =  digitalRead(8);
   delay(500);
   int c = 0;
-    c= digitalRead(5);
+    c= digitalRead(9);
   delay(500);
   int e=analogRead(A0);
-  if(e>30)
+  if(e<30)
   {
     if((d==1 && c==0)|| (d==0 && c==1))
-      digitalWrite(3,HIGH);
+      digitalWrite(10,1);
     else if(d==1 && c==1)
-       digitalWrite(2,1);
+       digitalWrite(11,1);
       else
     {
-      digitalWrite(3,0);
-       digitalWrite(2,0);
+      digitalWrite(10,0);
+       digitalWrite(11,0);
     }
   }
   else
   {
-    digitalWrite(3,0);
-       digitalWrite(2,0);
+    digitalWrite(10,0);
+       digitalWrite(11,0);
     }
     
     
